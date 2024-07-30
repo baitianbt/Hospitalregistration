4d725e9dad8e95ffe6c75444a227c1a827af4fcc
Gce <1749492810@qq.com>
2024-07-30 13:30:10 +0800

    添加项目文件。

---
+++ 合肥国医堂挂号/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace 合肥国医堂挂号
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labErrorMessage = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnQuery = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.webView);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(12, 14, 12, 14);
+            this.groupBox1.Size = new System.Drawing.Size(303, 494);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "国医堂";
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(12, 30);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(279, 450);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labErrorMessage);
+            this.groupBox2.Controls.Add(this.linkLabel1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.BtnQuery);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(307, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(450, 288);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "基础参数";
+            // 
+            // labErrorMessage
+            // 
+            this.labErrorMessage.AutoSize = true;
+            this.labErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.labErrorMessage.Location = new System.Drawing.Point(320, 66);
+            this.labErrorMessage.Name = "labErrorMessage";
+            this.labErrorMessage.Size = new System.Drawing.Size(91, 17);
+            this.labErrorMessage.TabIndex = 12;
+            this.labErrorMessage.Text = "errorMessage";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(392, 20);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(44, 17);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "未登录";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 10;
+            // 
+            // BtnQuery
+            // 
+            this.BtnQuery.Location = new System.Drawing.Point(229, 222);
+            this.BtnQuery.Name = "BtnQuery";
+            this.BtnQuery.Size = new System.Drawing.Size(102, 35);
+            this.BtnQuery.TabIndex = 9;
+            this.BtnQuery.Text = "查询挂号";
+            this.BtnQuery.UseVisualStyleBackColor = true;
+            this.BtnQuery.Click += new System.EventHandler(this.BtnQuery_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "预约挂号";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "李鑫"});
+            this.comboBox1.Location = new System.Drawing.Point(172, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(172, 114);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(133, 26);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton2.Location = new System.Drawing.Point(250, 159);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "下午";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton1.Location = new System.Drawing.Point(87, 159);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "上午";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(83, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "挂号日期:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(83, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "医    生:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(307, 292);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(450, 206);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "说明";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(4, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(442, 182);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 502);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.Text = "合肥国医堂挂号软件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button BtnQuery;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labErrorMessage;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ 合肥国医堂挂号/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
+using System.Net;
+using System.Web;
+
+namespace 合肥国医堂挂号
+{
+    public partial class Form1 : Form
+    {
+        // 网络校准时间API
+        private const string TimeServerUrl = "http://worldtimeapi.org/api/ip";
+        private HttpClient httpClient;
+        private List<CoreWebView2Cookie> storedCookies;
+        private System.Threading.Timer timer;
+        private string targetUrl;
+
+        public Form1()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+            storedCookies = new List<CoreWebView2Cookie>();
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            #region 初始化Web
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.Navigate("http://gyt.lpxxkj.com/index.php/Public/wx_login");
+            webView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+            #endregion
+
+            timer1.Start();
+        }
+
+        private async void CoreWebView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (e.IsSuccess)
+            {
+                // Execute JavaScript to set the zoom level to 50%
+                await webView.CoreWebView2.ExecuteScriptAsync("document.body.style.zoom = '50%';");
+
+                // Wait for some time to ensure login is completed
+                await Task.Delay(5000); // Adjust delay as needed
+
+                // Fetch cookies after login
+                await FetchCookies();
+            }
+            else
+            {
+                MessageBox.Show("Navigation failed.");
+            }
+        }
+
+
+        private async Task FetchCookies()
+        {
+            storedCookies = await webView.CoreWebView2.CookieManager.GetCookiesAsync("http://gyt.lpxxkj.com/index.php?m=&c=WxIndex&a=hos_list"); // Replace with your domain
+            
+        }
+
+        private  void timer1_Tick(object sender, EventArgs e)
+        {
+            // await UpdateTimeFromNetwork();
+            label2.Text = DateTime.Now.ToString();
+        }
+
+
+        private async Task UpdateTimeFromNetwork()
+        {
+            try
+            {
+                // Fetch time data from the network time server
+                var response = await httpClient.GetStringAsync(TimeServerUrl);
+                // Example response handling
+                dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(response);
+                string networkTime = json.datetime;
+
+                // Update the label with the network time
+                label2.Text = $"Current Time: {networkTime}";
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., network issues)
+                label2.Text = "Failed to retrieve time";
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private async void BtnQuery_Click(object sender, EventArgs e)
+        {
+           await SendRequestWithCookies("http://gyt.lpxxkj.com/index.php/wxIndex/my_order");
+        }
+
+
+
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <returns></returns>
+        private async Task SendRequestWithCookies(string url)
+        {
+            try
+            {
+                // Set cookies for HttpClient request
+                var handler = new HttpClientHandler();
+                var cookieContainer = new System.Net.CookieContainer();
+
+                foreach (var cookie in storedCookies)
+                {
+                    try
+                    {
+                        // URL encode the cookie value
+                        var cookieName = HttpUtility.UrlEncode(cookie.Name);
+                        var cookieValue = HttpUtility.UrlEncode(cookie.Value);
+
+                        // Print cookie details for debugging
+                        Console.WriteLine($"Adding cookie: Name={cookieName}, Value={cookieValue}, Domain={cookie.Domain}, Path={cookie.Path}");
+
+                        // Create a new System.Net.Cookie with the encoded name and value
+                        var netCookie = new Cookie(cookieName, cookieValue)
+                        {
+                            Domain = cookie.Domain,
+                            Path = cookie.Path
+                        };
+
+                        // Optional: Handle other properties if needed
+                        if (cookie.Expires != DateTime.MinValue)
+                        {
+                            netCookie.Expires = cookie.Expires;
+                        }
+
+                        cookieContainer.Add(new Uri(url), netCookie);
+                    }
+                    catch (CookieException ex)
+                    {
+                        // Handle invalid cookie value
+                        Console.WriteLine($"Invalid cookie: {cookie.Name}, Value: {cookie.Value}, Exception: {ex.Message}");
+                    }
+                }
+
+                handler.CookieContainer = cookieContainer;
+                var client = new HttpClient(handler);
+
+                // Send a request and get HTML content
+                var htmlContent = await client.GetStringAsync(url);
+
+                // Display the HTML content in WebView2
+                webView.CoreWebView2.NavigateToString(htmlContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send request or display content: {ex.Message}");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (linkLabel1.Text == "未登录")
+            {
+                webView.CoreWebView2.Navigate("http://gyt.lpxxkj.com/index.php/Public/wx_login");
+            }
+            else if(MessageBox.Show("请确定是否退出，将清空","是否退出",MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                storedCookies.Clear();
+                linkLabel1.Text = "未登录";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string doc_id = "";
+            string morOr = "";
+            string dateweek = "";
+            string date = "";
+
+            // http://gyt.lpxxkj.com/index.php?m=&c=wx_index&a=doc_reg&week=4&day=2&date=2024-07-19&doc_id=32056
+            if (comboBox1.Text == "李鑫")
+            {
+                doc_id = "32056";
+            }
+            if (radioButton1.Checked)
+            {
+                morOr = "1";
+            }
+            else
+            {
+                morOr = "2";
+            }
+            date = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            dateweek = dateTimePicker1.Value.DayOfWeek.ToString();
+
+
+             targetUrl = $"http://gyt.lpxxkj.com/index.php?m=&c=wx_index&a=doc_reg&week={dateweek}&day={morOr}&date={date}&doc_id={doc_id}";
+
+           DateTime targetTime = DateTime.Today.AddHours(8).AddMinutes(29).AddSeconds(58);
+            if (DateTime.Now > targetTime)
+            {
+                targetTime = targetTime.AddDays(1);
+            }
+
+            // Calculate the initial delay until the target time
+            TimeSpan initialDelay = targetTime - DateTime.Now;
+
+            // Set up the timer to trigger at the target time
+            timer = new System.Threading.Timer(TimerCallback, null, initialDelay, TimeSpan.FromHours(24));
+
+            button2.Enabled = false;
+        }
+
+        private void TimerCallback(object state)
+        {
+            Task.Run(() => SendRequestsWithCookies(targetUrl));
+        }
+
+
+        private async Task SendRequestsWithCookies(string PageUrl)
+        {
+            var handler = new HttpClientHandler();
+            var cookieContainer = new System.Net.CookieContainer();
+            foreach (var cookie in storedCookies)
+            {
+                try
+                {
+                    // URL encode the cookie value
+                    var cookieName = HttpUtility.UrlEncode(cookie.Name);
+                    var cookieValue = HttpUtility.UrlEncode(cookie.Value);
+
+                    // Print cookie details for debugging
+                    Console.WriteLine($"Adding cookie: Name={cookieName}, Value={cookieValue}, Domain={cookie.Domain}, Path={cookie.Path}");
+
+                    // Create a new System.Net.Cookie with the encoded name and value
+                    var netCookie = new Cookie(cookieName, cookieValue)
+                    {
+                        Domain = cookie.Domain,
+                        Path = cookie.Path
+                    };
+
+                    // Optional: Handle other properties if needed
+                    if (cookie.Expires != DateTime.MinValue)
+                    {
+                        netCookie.Expires = cookie.Expires;
+                    }
+
+                    cookieContainer.Add(new Uri(PageUrl), netCookie);
+                }
+                catch (CookieException ex)
+                {
+                    // Handle invalid cookie value
+                    Console.WriteLine($"Invalid cookie: {cookie.Name}, Value: {cookie.Value}, Exception: {ex.Message}");
+                }
+            }
+            handler.CookieContainer = cookieContainer;
+
+            var client = new HttpClient(handler);
+
+            var tasks = new List<Task>();
+
+            // Send multiple requests concurrently
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                tasks.Add(client.GetStringAsync(PageUrl));
+            }
+
+            try
+            {
+                await Task.WhenAll(tasks);
+                MessageBox.Show("All requests sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send requests: {ex.Message}");
+            }
+        }
+    }
+}
